318ae33ec44691e97b65bab5d79fe014764d6ffd
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-06-29 16:56:35 +0900

    03 반복문

---
+++ 03 반복문/25314.swift
@@ -0,0 +1,8 @@
+/* 코딩은 체육과목 입니다 */
+
+let n:Int = Int(readLine()!)!
+
+for _ in 1 ... n / 4 {
+    print("long", terminator: " ")
+}
+print("int")
---
+++ 03 반복문/2739.swift
@@ -0,0 +1,6 @@
+/* 구구단 */
+
+let input:Int = Int(readLine()!)!
+for i in 1 ... 9 {
+    print (input, "*", i, "=", input * i)
+}
