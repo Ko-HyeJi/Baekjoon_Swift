b6c06bb9a187073dc8da071d2d0ca67ff1278db6
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-06-29 16:52:34 +0900

    폴더 이름 변경

--- 01_입출력과 사칙연산/1000.swift
+++
@@ -1,4 +0,0 @@
-/* A+B */
-
-let numArr:Array = readLine()!.split(separator: " ")
-print(Int(numArr[0])! + Int(numArr[1])!)
--- 01_입출력과 사칙연산/11382.swift
+++
@@ -1,4 +0,0 @@
-/* 꼬마 정민 */
-
-let numArr:Array = readLine()!.split(separator: " ")
-print(UInt64(numArr[0])! + UInt64(numArr[1])! + UInt64(numArr[2])!)
